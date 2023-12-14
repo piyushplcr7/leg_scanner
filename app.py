f6295a6f83b161d300d933c15fb454ef6573f10a
Piyush Panchal <piyush.panchal@sam.math.ethz.ch>
2023-12-15 00:04:00 +0100

    On-device scan, CSV check added

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ from flask_sslify import SSLify
 app = Flask(__name__)
 sslify = SSLify(app)
 
+
 def process_image(imginput):
-    #img = cv2.imread('myimage.png')
+    # img = cv2.imread('myimage.png')
     img = cv2.imread(imginput)
     results = zxingcpp.read_barcodes(img)
     for result in results:
         print("Found barcode:\n Text:    '{}'\n Format:   {}\n Position: {}"
-            .format(result.text, result.format, result.position))
+              .format(result.text, result.format, result.position))
     if len(results) == 0:
         print("Could not find any barcode.")
     return results[0].text
+
 
 def search_str(file_path, word):
     with open(file_path, 'r') as file:
@@ -31,59 +33,120 @@ def search_str(file_path, word):
             print('string does not exist in a file')
             return False
 
+
 @app.route("/")
 def homepage():
     var = "poos"
-    return render_template('index.html',var=var)
+    return render_template('index.html', var=var)
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    print("POST request at /upload, received = > ",request.files)
+    print("POST request at /upload, received = > ", request.files)
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
     file = request.files['file']
-    print("Extracted file: ",file)
+    print("Extracted file: ", file)
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
     try:
-        #decoded = reader.decode(file)
+        # decoded = reader.decode(file)
         file_path = "image.png"
         file.save(file_path)
         result = process_image(file_path)
 
         # Finding the legi in csv
-        inCSV = search_str('students.csv',result)
+        inCSV = search_str('students.csv', result)
 
-        return jsonify({'result': result,'inCSV': inCSV})
+        return jsonify({'result': result, 'inCSV': inCSV})
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
+
 @app.route('/camera', methods=['POST'])
 def camera():
-    print("POST request at /camera, received => ",request.files)
+    print("POST request at /camera, received => ", request.files)
     if 'image' not in request.files:
         return jsonify({'error': 'No file part'})
 
     file = request.files['image']
-    print("Extracted file: ",file)
+    print("Extracted file: ", file)
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
     try:
-        #decoded = reader.decode(file)
+        # decoded = reader.decode(file)
         file_path = "screenshot.png"
         file.save(file_path)
         result = process_image(file_path)
 
         # Finding the legi in csv
-        inCSV = search_str('students.csv',result)
+        inCSV = search_str('students.csv', result)
 
-        return jsonify({'result': result,'inCSV': inCSV})
+        return jsonify({'result': result, 'inCSV': inCSV})
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
+
+@app.route('/leginr', methods=['POST'])
+def leginr():
+    print("POST request received")
+    # try:
+    # Decode JSON data from the request body
+    data = request.json
+
+    # Assuming data contains a key named 'decodedBarcode'
+    decoded_barcode = data.get('legi_nr')
+
+    decoded_barcode = decoded_barcode[1:]
+
+    print('decoded_barcode = ', decoded_barcode)
+
+    found = False
+    # Read the CSV file and find the corresponding names
+    with open('students.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['student_id'] == decoded_barcode:
+                first_name = row['first_name']
+                last_name = row['last_name']
+                print(f"First Name: {first_name}, Last Name: {last_name}")
+                found = True
+                break
+        else:
+            print(f"Student ID {decoded_barcode} not found.")
+
+    # Response
+    if found:
+        response_data = {
+            'result': 'found',
+            'first_name': first_name,
+            'last_name': last_name
+        }
+    else:
+        response_data = {
+            'result': 'not_found',
+            'first_name': 'null',
+            'last_name': 'null'
+        }
+
+    print(response_data)
+
+    return jsonify(response_data)
+
+    """ except Exception as e:
+        # Handle any errors that might occur during decoding or processing
+        error_message = str(e)
+        return jsonify({'error': error_message}), 500 """
+
+
 if __name__ == "__main__":
-    #app.run(debug=True, ssl_context=('cert.pem', 'priv_key.pem'))
-    app.run(debug=True)
+    # app.run(debug=True, ssl_context=('cert.pem', 'priv_key.pem'))
+    # app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    """ app.run(debug=True, host='0.0.0.0', port=5000,
+            ssl_context=('server.crt', 'server.key')) """
+    app.run(debug=True, host='0.0.0.0', port=5000,
+            ssl_context=('server.crt', 'server.key'))
